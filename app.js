cbd548ee16e96cfbc9ad2f93886465f1f673037d
Balu Tirupati <balusekhar8@gmail.com>
2024-05-23 16:49:40 -0500

    Used Joi for Validation

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-const Listing = require("./models/listing")
-const path = require("path")
-const methodOvveride = require("method-override")
-const ejsMate = require("ejs-mate")
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOvveride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+const {listingSchema} = require("./schema")
 
 main()
   .then(() => {
-    console.log("Mongo Connection Successful");
+    console.log("Mongo Consction Successful");
   })
   .catch((err) => console.log(err));
 
@@ -16,84 +19,121 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/BYOB");
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOvveride("_method"));
 // use ejs-locals for all ejs templates:
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-// app.get("/test",async (req,res)=>{
-//     let testListing = new Listing({
-//         title: "New Villa",
-//         description: "Villa desc",
-//         price: 1200,
-//         location:"Goa",
-//         country:"India"
-//     });
-//     await testListing.save();
-//     res.send("Test Success")
-// })
-
-app.get("/listings",async(req,res)=>{
-    const allListings = await Listing.find({})
-    res.render("listings/index.ejs",{allListings})
-})
-
-app.post("/listings",async(req,res)=>{
-    try{
-        const {title,description,image,price,location,country} = req.body
-        let newListing = new Listing({
-            title,description,image,price,location,country
-        })
-        await newListing.save()
-        console.log("Sucess");
-        res.redirect("/listings")
-    }catch(error){
-        console.log(error);
+const validateListing = (req,res,next) => {
+    let {error} = listingSchema.validate(req.body)
+    if(error){
+        throw new ExpressError(400,error)
+    }else{
+        next()
     }
-})
+}
 
-app.get("/listings/new",async(req,res)=>{
-    res.render("listings/new.ejs")
-})
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
-app.get("/listings/:id",async(req,res)=>{
-    const id = req.params.id
-    const specificListing = await Listing.findById(id)
-    res.render("listings/show.ejs",{specificListing})
-})
+//create
+app.post(
+  "/listings", validateListing,
+  wrapAsync(async (req, res, next) => {
+    const { title, description, image, price, location, country } = req.body;
+
+    let newListing = new Listing({
+      title,
+      description,
+      image,
+      price,
+      location,
+      country,
+    });
+    await newListing.save();
+    console.log("Sucess");
+    res.redirect("/listings");
+  })
+);
+
+app.get(
+  "/listings/new",
+  wrapAsync(async (req, res) => {
+    res.render("listings/new.ejs");
+  })
+);
+
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const id = req.params.id;
+    const specificListing = await Listing.findById(id);
+    res.render("listings/show.ejs", { specificListing });
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-  });
+  })
+);
 
 //update
-app.put("/listings/:id", async (req, res) => {
+app.put(
+  "/listings/:id", validateListing,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     const { title, description, image, price, location, country } = req.body;
-    await Listing.findByIdAndUpdate(id, { title, description, image, price, location, country });
+    await Listing.findByIdAndUpdate(id, {
+      title,
+      description,
+      image,
+      price,
+      location,
+      country,
+    });
     res.redirect(`/listings/${id}`);
-});
+  })
+);
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-  });
+  })
+);
 
+app.get("/", (req, res) => {
+  res.send("I am Home");
+});
 
-app.get("/",(req,res)=>{
-    res.send("I am Home");
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
 
-app.listen("3000",()=>{
-    console.log("Server is listening on Port 3000");
-})
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  //   res.status(statusCode).send(message);
+  res.status(statusCode).render("./listings/error.ejs",{message});
+});
+
+app.listen("3000", () => {
+  console.log("Server is listening on Port 3000");
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# BYOB - Book Your Own Bed
+
+This project is a web-based platform designed to manage listings for properties or services. It's built with Express.js and utilizes Mongoose for database operations and Joi for request validation, ensuring robust data handling and validation.
+
+## Features
+
+- **CRUD Operations**: Create, read, update, and delete listings through a RESTful API.
+- **Data Validation**: Integrated Joi validation to ensure the integrity of incoming data.
+- **Responsive Web Design**: Designed to be responsive and accessible on a variety of devices and screen sizes.
+
+## Technologies Used
+
+- **Node.js**
+- **Express.js**: Web application framework for Node.js.
+- **MongoDB/Mongoose**: NoSQL database with Mongoose as an ODM.
+- **Joi**: Object schema description language and validator for JavaScript objects.
+- **EJS**: Templating engine to render HTML from templates.
+
